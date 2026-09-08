9ea212b50ac3781fc617b68d771e482ca52ede16
Fernando <91239897+FerNinja444@users.noreply.github.com>
2026-09-08 19:32:30 -0400

    Add files via upload

---
+++ dados.php
@@ -0,0 +1,41 @@
+<?php
+
+$estoque = array (
+  0 => 
+  array (
+    'nome' => 'Teclado Membrana',
+    'preco' => 50.0,
+    'quantidade' => 5,
+    'categoria' => 'Perifericos',
+  ),
+  1 => 
+  array (
+    'nome' => 'Mouse Gamer',
+    'preco' => 150.0,
+    'quantidade' => 10,
+    'categoria' => 'Perifericos',
+  ),
+  2 => 
+  array (
+    'nome' => 'Monitor 24\'\'',
+    'preco' => 800.0,
+    'quantidade' => 3,
+    'categoria' => 'Monitores',
+  ),
+  3 => 
+  array (
+    'nome' => 'Notebook i5',
+    'preco' => 2500.0,
+    'quantidade' => 2,
+    'categoria' => 'Computadores',
+  ),
+  4 => 
+  array (
+    'nome' => 'Gabinete Gamer C3-TECH',
+    'preco' => 259.8999999999999772626324556767940521240234375,
+    'quantidade' => 7,
+    'categoria' => 'Gabinetes',
+  ),
+);
+
+?>
---
+++ rodape.php
@@ -0,0 +1,5 @@
+<footer>
+        <p>&copy; 2026 - Sistema desenvolvido em PHP</p>
+    </footer>
+</body>
+</html>
