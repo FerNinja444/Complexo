a7007e4131895a305320f8d7df7ad5b2aad267dc
Fernando <91239897+FerNinja444@users.noreply.github.com>
2026-09-08 19:32:07 -0400

    Delete Ex000 directory

--- Ex000/menu.php
+++
@@ -1,5 +0,0 @@
-<nav style="background: #2c3e50; padding: 10px; text-align: center;">
-    <a href="index.php" style="color: white; margin: 0 15px; text-decoration: none;">Início</a>
-    <a href="produtos.php" style="color: white; margin: 0 15px; text-decoration: none;">Produtos</a>
-    <a href="cadastrarProduto.php" style="color: white; margin: 0 15px; text-decoration: none;">Cadastrar Produto</a>
-</nav>
--- Ex000/rodape.php
+++
@@ -1,5 +0,0 @@
-<footer>
-        <p>&copy; 2026 - Sistema desenvolvido em PHP</p>
-    </footer>
-</body>
-</html>
